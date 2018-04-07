7daac07063cf8fd9b0cfba36c9e6c9d198860295
inhochoi <inhochoi@coupang.com>
2018-04-07 11:00:32 +0900

    first commit

---
+++ src/main/java/com/deprommet/mini5/api/MiniTeam5ApiApplication.java
@@ -0,0 +1,12 @@
+package com.deprommet.mini5.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTeam5ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTeam5ApiApplication.class, args);
+	}
+}
